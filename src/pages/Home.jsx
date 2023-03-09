35fa98c91582a7a54139bb66567721f334acda41
guardiangel <felix.sgq@gmail.com>
2023-03-09 00:09:54 -0500

    Slider.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Announcement />
       <NavBar />
+      <Slider />
     </div>
   );
 };
