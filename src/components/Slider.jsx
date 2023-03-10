04a5a9aa484ef253befb20b37e966a4e156793af
guardiangel <felix.sgq@gmail.com>
2023-03-09 23:55:04 -0500

    Adjust image position

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,8 +5,9 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: gray;
+
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -23,15 +24,100 @@ const Arrow = styled.div`
   margin: auto;
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(
+    0vw
+  ); //move in horizontal direction, 0vw will display the first image
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+  width: 200%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  const handleClick = (direction) => {};
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>{" "}
-      <Arrow direction="right">
+      <Wrapper>
+        <Slide bg="f5fafd">
+          <ImageContainer>
+            <Image src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" />
+          </ImageContainer>
+          <InfoContainer>
+            <Title>Summer sales</Title>
+            <Description>Get flat 30% off for new arrivals</Description>
+            <Button>Show now</Button>
+          </InfoContainer>
+        </Slide>
+
+        <Slide bg="fcf1ed">
+          <ImageContainer>
+            <Image src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" />
+          </ImageContainer>
+          <InfoContainer>
+            <Title>Winter sales</Title>
+            <Description>Get flat 30% off for new arrivals</Description>
+            <Button>Show now</Button>
+          </InfoContainer>
+        </Slide>
+
+        <Slide bg="fbf0f4">
+          <ImageContainer>
+            <Image src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" />
+          </ImageContainer>
+          <InfoContainer>
+            <Title>Popular sales</Title>
+            <Description>Get flat 30% off for new arrivals</Description>
+            <Button>Show now</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
